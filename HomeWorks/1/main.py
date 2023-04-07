5217b3b819bbf106b5400d168048e8b0a426e260
roman lepihov <lepihovrom@yandex.by>
2023-04-07 21:33:33 +0300

    add: homework 1 task 4

--- HomeWorks/1/main.py
+++ HomeWorks/1/main.py
@@ -1,5 +1,6 @@
 #task 2
 
+"""
 while (True):
     str = input("Введите трехзначное число: ")
 
@@ -11,5 +12,22 @@ while (True):
 sumNumbers = int(str[0]) + int(str[1]) + int(str[2])
 
 print(f"Сумма цифр числа {str} равна {sumNumbers} ({int(str[0])} + {int(str[1])} + {int(str[2])})")
+"""
 
+#task 4
 
+s = int(input("Введите общее число журавликов: "))
+
+# petr = x
+# serg = x
+# kate = 2*(x+x) = 4*x
+# s = petr + serg + kate = 6*x
+
+if s % 6 != 0:
+    print("Такое число журавливков согласно условию сделать бы не смогли!!!")
+else:
+    x = int(s / 6)
+
+    print (f"Петя сделал: {x}")
+    print (f"Сережа сделал: {x}")
+    print (f"Катя сделала: {4*x}")
