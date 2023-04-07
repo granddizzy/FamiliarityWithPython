4913d3a978c56c84aac5e2924b5a9310c2257bb2
roman lepihov <lepihovrom@yandex.by>
2023-04-07 20:52:29 +0300

    add: homework1 task2

---
+++ HomeWorks/1/main.py
@@ -0,0 +1,16 @@
+#task 2
+
+while (True):
+    str = input("Введите трехзначное число: ")
+
+    if len(str) > 3 or len(str) < 3:
+        print ("Число не трехзначное!!!")
+    else:
+        break
+
+sumNumbers = int(str[0]) + int(str[1]) + int(str[2])
+
+print(f"Сумма цифр числа {str} равна {sumNumbers}")
+
+
+
