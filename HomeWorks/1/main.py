be832b996ae0ca66a4fa53ddef0e04d33f6a0360
roman lepihov <lepihovrom@yandex.by>
2023-04-07 22:14:33 +0300

    fix: homework 1 task 2

--- HomeWorks/1/main.py
+++ HomeWorks/1/main.py
@@ -1,18 +1,17 @@
 #task 2
 
-"""
 while (True):
     str = input("Введите трехзначное число: ")
 
-    if len(str) > 3 or len(str) < 3:
-        print ("Число не трехзначное!!!")
+    if not str.isdigit() or len(str) > 3 or len(str) < 3:
+        print("Не правильно указано число!!!")
     else:
         break
 
 sumNumbers = int(str[0]) + int(str[1]) + int(str[2])
 
 print(f"Сумма цифр числа {str} равна {sumNumbers} ({int(str[0])} + {int(str[1])} + {int(str[2])})")
-"""
+
 
 #task 4
 
@@ -36,6 +35,7 @@ else:
 
 # task 6
 
+"""
 while (True):
     num = int(input("Введите 6 значный номер билета:"))
 
@@ -51,3 +51,4 @@ if sumLleft == sumRight:
     print("Это число СЧАСТЛИВОЕ!!!")
 else:
     print("Это не счастливое число.")
+"""
