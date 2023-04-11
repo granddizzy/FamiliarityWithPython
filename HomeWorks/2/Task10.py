0de22401de456cc677dfeeef7ca3003686d92182
roman lepihov <lepihovrom@yandex.by>
2023-04-11 21:30:55 +0300

    fix task10 homework 2

--- HomeWorks/2/Task10.py
+++ HomeWorks/2/Task10.py
@@ -11,3 +11,20 @@ coins = [randint(0, 1) for i in range(n)]
 print(coins)
 
 print(f"Минимальное количество монеток которое нужно перевернуть {coins.count(0) if coins.count(0) <= coins.count(1) else coins.count(1)}")
+
+# через циклы
+count_up = 0
+count_down = 0
+for i in coins:
+    if i == 0:
+        count_up += 1
+    else:
+        count_down += 1
+
+if count_up > count_down:
+    max_count = count_down
+else:
+    max_count = count_up
+
+print(f"Минимальное количество монеток которое нужно перевернуть {max_count}")
+
