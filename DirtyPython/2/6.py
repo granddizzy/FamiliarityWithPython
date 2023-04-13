4ba53ced46f269db95e0e6599b342540ee513f4a
roman lepihov <lepihovrom@yandex.by>
2023-04-13 15:10:30 +0300

    add DirtyPython homework 2

---
+++ DirtyPython/2/6.py
@@ -0,0 +1,80 @@
+# 6. ПРОСТЕЙШИЙ КАЛЬКУЛЯТОР
+# Написать программу, которая выполняет над двумя вещественными числами одну из четырех арифметических операций (сложение, вычитание, умножение или деление):
+# вводим первое число,
+# потом операцию
+# и второе число
+# выводится результат
+#
+# Программа должна завершаться только по желанию пользователя: можно ввести enter и программа закончится, или еще операцию и еще число. Ну и помним, что на ноль делить нельзя.
+
+flag = True
+first_num, second_num, oper = "", "", ""
+text = "Введите первое число"
+
+from decimal import Decimal
+
+while flag:
+    if not first_num or not oper or not second_num:
+        input_str = input(text + ": ").replace(" ", "")
+
+    if input_str:
+        if not first_num:
+            if input_str.count(".") > 1 or input_str.count("-") > 1 or input_str.find(".") == len(input_str)-1:
+                print("И как это понять ?")
+            else:
+                i = 0
+                for symbol in input_str:
+                    if not symbol.isdigit():
+                        if (i == 0 and symbol != "-" and symbol != ".") or (i > 0 and symbol != "."):
+                            print("Дурачек что-ли ?")
+                            break
+                    i += 1
+                else:
+                    first_num = input_str
+                    text = "Введите операцию (+ - * /)"
+
+        elif not oper:
+            if len(input_str) > 1:
+                print("И как это понять ?")
+            else:
+                for symbol in input_str:
+                    if symbol != "+" and symbol != "-" and symbol != "*" and symbol != "/":
+                        print("Дурачек что-ли ?")
+                        break
+                else:
+                    oper = input_str
+                    text = "Введите второе число"
+        elif not second_num:
+            if oper == "/" and len(input_str) == 1 and input_str == "0":
+                print("0? Издеваешься ? За тобой уже выехали !!!")
+            else:
+                if input_str.count(".") > 1 or input_str.count("-") > 1 or input_str.find(".") == len(input_str)-1:
+                    print("И как это понять ?")
+                else:
+                    i = 0
+                    for symbol in input_str:
+                        if not symbol.isdigit():
+                            if (i == 0 and symbol != "-" and symbol != ".") or (i > 0 and symbol != "."):
+                                print("Дурачек что-ли ?")
+                                break
+                        i += 1
+                    else:
+                        second_num = input_str
+                    text = "Введите первое число"
+        else:
+            if oper == "+":
+                res = Decimal(first_num) + Decimal(second_num)
+            elif oper == "-":
+                res = Decimal(first_num) - Decimal(second_num)
+            elif oper == "*":
+                res = Decimal(first_num) * Decimal(second_num)
+            elif oper == "/":
+                res = Decimal(first_num) / Decimal(second_num)
+
+            print(f"Результат: {Decimal(first_num)} {oper} {Decimal(second_num)} = {res}")
+
+            first_num, oper, second_num = "", "", ""
+    else:
+        flag = False
+else:
+    print("Всем спасибо - все свободны")
