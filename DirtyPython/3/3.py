0f965b8e1e368956983e05bacc9e5897458b8f62
roman lepihov <lepihovrom@yandex.by>
2023-04-18 21:31:29 +0300

    update DirtyPython HomeWork 3 Task 3

--- DirtyPython/3/3.py
+++ DirtyPython/3/3.py
@@ -2,13 +2,21 @@
 # написать программу, которая определяет цвет клетки по ее координатам (например B7 = Черная), если точно знаем, что клетка А1 - черная
 
 alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-
 y, x = list(input("Введите координаты клетки: "))
+print("Черная" if ((int(x) + 1) % 2 if not (alphabet.index(y.lower()) + 1) % 2 else int(x) % 2) else "Белая")
 
-y_num = alphabet.index(y.lower()) + 1
-x_num = int(x)
 
-if not y_num % 2:
-    x_num += 1
+# понятнее
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+y = alphabet.index(y.lower()) + 1
+x = int(x)
 
-print("Черная" if x_num % 2 else "Белая")
+print("Черная" if ((x + 1) % 2 if not y % 2 else x % 2) else "Белая")
+
+
+# еще понятнее
+# if not y % 2:
+#      x += 1
+#
+# print("Черная" if x % 2 else "Белая")
+
