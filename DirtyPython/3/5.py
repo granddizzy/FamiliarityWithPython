1820a7ae3106720c9812535fb97333e6e51a62d3
roman lepihov <lepihovrom@yandex.by>
2023-04-19 17:06:24 +0300

    update DirtyPython HomeWork 3 Task 5

--- DirtyPython/3/5.py
+++ DirtyPython/3/5.py
@@ -5,8 +5,10 @@
 # not year % 4 or (not year % 100 and not year % 400):
 # 365 ... 366
 
+#from datetime import datetime
+
 def is_leap_year(year):
-    return not year % 4 or (not year % 100 and not year % 400)
+    return not year % 4 and year % 100 or not year % 400
 
 
 week_days = {1: "Понедельник", 2: "Вторник", 3 : "Среда", 4 : "Четверг", 5: "Пятница", 6: "Суббота", 0: "Воскресенье"}
@@ -18,21 +20,29 @@ begin_day, begin_month, begin_year = list(map(int, "01.07.1970".split(".")))
 input_date = input("Введите дату DD.MM.YYYY: ")
 end_day, end_month, end_year = list(map(int, input_date.split(".")))
 
+# если начальная дата больше конечной, то меняем их местами
+reverse = False
+if begin_year > end_year or begin_year == end_day and begin_month > end_month or begin_year == end_year and begin_month == end_month and begin_day > end_day:
+    t_end_day, t_end_month, t_end_year = end_day, end_month, end_year
+    end_day, end_month, end_year = begin_day, begin_month, begin_year
+    begin_day, begin_month, begin_year = t_end_day, t_end_month, t_end_year
+    reverse =  True
+
 diff_days = 0
 
-# подведем к началу следующего года (если стартовая дата не с начала года)
+# подведем к началу следующего года (если начальная дата не с начала года)
 if begin_day != 1 or begin_month != 1:
     leapyear_repair = 1 if is_leap_year(begin_year) else 0
 
     # добавляем дни месяца
-    diff_days += month_days[begin_month] + leapyear_repair if begin_month == 2 else 0
+    diff_days += month_days[begin_month] + (leapyear_repair if begin_month == 2 else 0) - begin_day + 1
 
     # добавляем оставшиеся месяцы
     for month in range(begin_month + 1, 13):
-        diff_days += month_days[month] + leapyear_repair if month == 2 else 0
+        diff_days += month_days[month] + (leapyear_repair if month == 2 else 0)
 
 # количество дней полных лет
-diff_days = sum([366 if is_leap_year(year) else 365 for year in range(begin_year, end_year)])
+diff_days += sum([366 if is_leap_year(year) else 365 for year in range(begin_year, end_year - 1)])
 
 leapyear_repair = 1 if is_leap_year(end_year) else 0
 
@@ -41,7 +51,11 @@ for month in range(1, end_month):
     diff_days += month_days[month] + (leapyear_repair if month == 2 else 0)
 
 # количество дней в неполном месяце и корректировка на день недели
-diff_days += end_day - day_of_the_week
+diff_days += end_day - 1
+
+#diff_days2 = (datetime(end_year, end_month, end_day) - datetime(begin_year, begin_month, begin_day)).days
+
+diff_days -= day_of_the_week - (1 if reverse else 0)
 
 week_day = diff_days % 7 if diff_days > 6 else diff_days
 
