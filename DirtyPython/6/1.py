582ef7d4d161d317d0e451bf44d3cd0874a99b5a
roman lepihov <lepihovrom@yandex.by>
2023-04-30 19:56:52 +0300

    update DirtyPython 6

--- DirtyPython/6/1.py
+++ DirtyPython/6/1.py
@@ -2,7 +2,7 @@ with open("sample.txt", "r", encoding="UTF-8") as file:
     text_ = file.read()
 
 new_text = ". ".join(
-    map(lambda str_: str_[0:1].upper() + str_[1: len(str_)], text_.split(". ")))
+    map(lambda str_: str_.capitalize(), text_.split(". ")))
 
 with open("sample_new.txt", "w", encoding="UTF-8") as file:
     print(new_text, file=file, end="")
--- DirtyPython/6/2.py
+++ DirtyPython/6/2.py
@@ -4,18 +4,19 @@ import string
 with open("Alice in Wonderland.txt", "r", encoding="UTF-8") as file:
     text_ = file.read().lower()
 
-for sym in string.punctuation + "\n\xa0" + "1234567890«»-–":
-    text_ = text_.replace(sym, "")
+for sym in string.punctuation + "\n\xa01234567890«»-–„":
+    text_ = text_.replace(sym, " ")
 
 words = text_.split()
-dict_words = {word: words.count(word) for word in set(words)}
+dict_words = {word: words.count(word) for word in set(words) if len(word) > 4}
 
-# print(max(dict_words, key=lambda k: dict_words[k]))
+sorted_tuples = sorted(dict_words.items(), key=lambda el: el[1])
+length = len(sorted_tuples)
 
-# sorted_tuples = sorted(dict_words.items(), key=lambda el: el[1])
 # for item in sorted_tuples:
-#     print(*item, sep=" - ")
+#     if item[0].find("алиса") >= 0:
+#         print(*item, sep=" - ")
 
-sorted_dict = dict(sorted(dict_words.items(), key=lambda el: el[1]))
-for k, v in sorted_dict.items():
-    print(k, "-", v)
+
+for i in range(length - 5, length):
+    print(*sorted_tuples[i], sep=" - ")
