2b1acd2636f31a7e018153dde09b037ce7e9cdaf
roman lepihov <lepihovrom@yandex.by>
2023-05-18 21:04:21 +0300

    del DirtyPython

--- DirtyPython/6/1.py
+++
@@ -1,8 +0,0 @@
-with open("sample.txt", "r", encoding="UTF-8") as file:
-    text_ = file.read()
-
-new_text = ". ".join(
-    map(lambda str_: str_.capitalize(), text_.split(". ")))
-
-with open("sample_new.txt", "w", encoding="UTF-8") as file:
-    print(new_text, file=file, end="")
